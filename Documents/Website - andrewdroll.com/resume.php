7454aa68049a754bb45071cccc92827f9d662d4a
Datah <andrew.d.droll@gmail.com>
2014-11-25 13:51:25 -0500

    New webpage

---
+++ Documents/Website - andrewdroll.com/resume.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+	<base href="http://www.datah.net/"/>
+	<?php include 'fonts.html' ;?>
+	<link type="text/css" rel="stylesheet" href="resume.css"/>
+	<title>Andrew Droll - Live Resume</title>
+	<link rel="shortcut icon" href="/images/PiIcon.ico" type="image/x-icon" />
+</head>
+
+<body>
+
+	<div class="content" style="width:75%">
+		<div class="header">
+			<div class="nav">
+				<div class="container">
+					<?php include 'http://www.datah.net/nav.html' ;?>
+				</div>
+			</div>
+		</div>
+	<!-- end header -->
+
+	<!-- header -->
+
+
+
+
+	<div class="mainbody">
+	<!-- All content in main pane should go in this div -->
+	
+		<!--can change to class=right-wide if the wide pane should be right -->
+			<div class="intro">
+				<h1>Andrew Droll</h1>
+				<div class="hcenter">
+					<p>Consultant, Educator, Researcher</p>
+				</div>
+				<div class="hleft">
+					<p>613.864.0442</p>
+					<p><a href="mailto:andrew.d.droll@gmail.com">andrew.d.droll@gmail.com</a></p>
+				</div>
+
+				<div class="hright">
+					<p><a href="http://www.andrewdroll.com" target="_blank">andrewdroll.com</p>
+					<p><a href="http://www.linkedin.com/in/andrewdroll" target="_blank">linkedin.com/in/andrewdroll</p>
+					
+				</div>
+			</div>
+			INSERT MORE CONTENT
+
+
+
+<!-- Right panes start here -->
+<!-- can change to class = left if the narrow pane should be left -->
+		<!--<div class="right">
+			INSERT CONTENT
+
+		
+			<div class="container">
+				<h2>SAMPLE RIGHT HEADER<h2>
+			</div>
+			<div class="container">
+				SAME CONTAINER
+			</div>
+
+		</div>-->
+<!-- End right panes -->
+
+
+<!-- Close mainbody div -->
+	</div>
+	
+<!--Footer -->
+	<div class="footer"> 
+	</div>
+
+</div>
+
+</body>
+</html>
---
+++ Documents/Website - andrewdroll.com/template.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+	<base href="http://www.datah.net/"/>
+	<?php include 'fonts.html' ;?>
+	<link type="text/css" rel="stylesheet" href="mainstyle.css"/>
+	<title>Andrew Droll - Template</title>
+	<link rel="shortcut icon" href="/images/PiIcon.ico" type="image/x-icon" />
+</head>
+
+<body>
+
+<div class="content" style="width:960px">
+
+	<!-- header -->
+	<div class="header">
+		
+		<div class="titleimg" style="margin:auto;background-image:url(INSERT URL);height:315px;width:960px;padding:0px">
+			<h1 id="pagehead">andrewdroll.com</h1>
+		</div>
+	
+
+		<div class="nav">
+			<div class="container">
+				<?php include 'http://www.datah.net/nav.html' ;?>
+			</div>
+		</div>
+
+	</div>
+	<!-- end header -->
+
+	<div class="mainbody">
+	<!-- All content in main pane should go in this div -->
+	
+		<div class="left-wide">
+		<!--can change to class=right-wide if the wide pane should be right -->
+			<div class="intro">
+				INSERT CONTENT
+			</div>
+			INSERT MORE CONTENT
+		</div>
+
+
+
+<!-- Right panes start here -->
+<!-- can change to class = left if the narrow pane should be left -->
+		<div class="right">
+			INSERT CONTENT
+
+		
+			<div class="container">
+				<h2>SAMPLE RIGHT HEADER<h2>
+			</div>
+			<div class="container">
+				SAME CONTAINER
+			</div>
+
+		</div>
+<!-- End right panes -->
+
+
+<!-- Close mainbody div -->
+	</div>
+	
+<!--Footer -->
+	<div class="footer"> 
+		<?php include 'http://www.datah.net/footer.html' ;?>
+	</div>
+
+</div>
+
+</body>
+</html>
